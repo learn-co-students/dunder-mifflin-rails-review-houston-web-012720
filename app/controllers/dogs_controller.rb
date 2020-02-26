2a8948aa7255f4704936c6485eaa0481a9366867
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-26 15:36:34 -0600

    Munder Dufflin Done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        # @employees = Employee.all
+        @owners = Employee.select{|employee| employee.dog == @dog}
+    end
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,56 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = current_employee
+    end
+
+    def edit
+        @employee = current_employee
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee = current_employee
+        # byebug
+        if @employee.valid?
+            @employee.update(employee_params)
+            redirect_to @employee
+        else
+            # flash[:errors] = employee.errors.full_messages
+            # byebug
+            redirect_to @employee
+        end
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        # byebug
+        employee = Employee.new(employee_params)
+        if employee.valid?
+            # byebug
+            employee.save
+            redirect_to employee
+        else
+            # flash[:errors] = employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    private
+    
+    def current_employee
+        Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+    end
 end
